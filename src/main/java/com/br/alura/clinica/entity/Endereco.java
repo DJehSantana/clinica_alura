2b5f5e2bb1197940f5dbf1e14bdfe28332d8fdbe
Jessica Helena Dias Santana <jhsantana@stefanini.com>
2023-05-18 14:22:54 -0300

    Criando entidade medico

---
+++ src/main/java/com/br/alura/clinica/entity/Endereco.java
@@ -0,0 +1,22 @@
+package com.br.alura.clinica.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private Long id;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+
+}
